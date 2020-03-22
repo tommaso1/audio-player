eebd608456f8f1f06b192d1ef4c0123e4a1f35f7
tomma <tommasorosso1@gmail.com>
2020-03-22 20:15:31 +0100

    initial commit

---
+++ docs/sw.js
@@ -0,0 +1,53 @@
+var CACHE_NAME = 'cache-v1';
+var urlsToCache = [
+  '/audio-player/',
+  '/audio-player/main.css',
+  '/audio-player/main.js',
+  '/audio-player/when.mp3',
+];
+
+self.addEventListener('install', function(event) {
+    console.log('Service worker -> Install');
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Service worker -> Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        console.log('Service worker -> Cache hit');
+        // Cache hit - return response
+        if (response) {
+            return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+          // Check if we received a valid response
+          if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+          }
+
+          var responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+              .then(function(cache) {
+              cache.put(event.request, responseToCache);
+              });
+
+          console.log('Service worker -> Cache storing value');
+
+          return response;
+          }
+        );
+      })
+    );
+  });
+  
---
+++ src/Main.elm
@@ -0,0 +1,105 @@
+port module Main exposing (..)
+
+import Html exposing (..)
+import Browser
+import Time
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
+
+port playNotification : Bool -> Cmd msg
+
+-- MAIN
+
+
+main =
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { seconds: Int,
+    playerState: PlayerState
+  }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( { seconds = 0, playerState = Idle }
+  , Cmd.none
+  )
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Tick Time.Posix
+  | PlayAudio
+  | PauseAudio
+
+type PlayerState = Idle | Play | Stop
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model = case (msg, model.playerState) of
+          (Tick _, Play) -> ( { model | seconds = model.seconds + 1 }, Cmd.none)
+          (Tick _, _) -> ( model , Cmd.none)
+          (PlayAudio, _) -> ({ model | playerState = Play }, playNotification True)
+          (PauseAudio, _) -> ({ model | playerState = Stop }, playNotification False)
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every 1000 Tick
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  let
+    second = formatSeconds(modBy 60 model.seconds)
+    minutes = formatSeconds(model.seconds // 60)
+  in
+  div [ class "container" ] [
+      div [ class "circle-container" ] [
+        div [ class (classFromModel model) ] [
+          h1 [] [ text (minutes ++ ":" ++ second) ]
+        ]
+      ]
+      , button [ onClick
+       (case model.playerState of 
+        Idle -> PlayAudio
+        Play -> PauseAudio
+        Stop -> PlayAudio)
+      , id "input"] [
+        text (case model.playerState of 
+          Idle -> "Play"
+          Play -> "Pause"
+          Stop -> "Play"
+        )
+      ]
+  ]
+
+classFromModel : Model -> String
+classFromModel model = case model.playerState of
+       Idle -> "init"
+       Stop -> "stop"
+       Play -> "play"
+        
+formatSeconds : Int -> String
+formatSeconds i = if i > 9 then String.fromInt i else "0" ++ String.fromInt i
